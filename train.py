e2a4a9229855da00648f70ee5ad54e48bae90906
Dark Coder <codewithdark90@gmail.com>
2025-01-04 16:19:22 +0500

    Update train.py

--- train.py
+++ train.py
@@ -1,149 +1,113 @@
-import torch.optim as optim
-import torch.nn as nn
 import torch
-import numpy as np
-import matplotlib.pyplot as plt
-from torchviz import make_dot
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, models
 from torchsummary import summary
-import itertools
-from sklearn.metrics import confusion_matrix
+import numpy as np
 import tqdm
 import logging
+import os
+import logging
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+import makedot
 
-class Train:
-    
-    def __init__(self, model, device, train_loader, batch_size, num_epochs=2, learning_rate=0.01, model_name=None):
-        self.model = model
-        self.model_name = model_name or 'Untitled'
-        self.device = device
-        self.train_loader = train_loader
-        self.num_epochs = num_epochs
+
+
+class PneumoniaTrainer:
+    """
+    A class for training a CNN model for pneumonia detection.
+    """
+    def __init__(self, model, model_name, img_size=50, batch_size=100, learning_rate=0.001, epochs=2, model_path="Model/model.pth"):
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.img_size = img_size
+        self.batch_size = batch_size
         self.learning_rate = learning_rate
-        self.batch_size= batch_size
+        self.epochs = epochs
+        self.model_path = model_path
+        self.model_name = model_name
 
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
-
-        self.train_losses = []  # To store training losses
-        self.train_accuracies = []  # To store training accuracies
-        self.validation_accuracies = []  # To store validation accuracies
-
-
-        self.logging = logging.basicConfig(
-                level=logging.INFO,
-                format="%(asctime)s - %(levelname)s - %(message)s",
-                handlers=[
-                    logging.FileHandler(f"{self.model_name}.log"),
-                    logging.StreamHandler(),
-                ],
-            )
+        # Initialize model, optimizer, and loss function
+        if model is None:
+            raise ValueError("Please provide a valid model instance.")
+        self.net = model.to(self.device)
+        self.optimizer = optim.Adam(self.net.parameters(), lr=self.learning_rate)
+        self.loss_function = nn.CrossEntropyLoss()
 
 
-    def trainer(self):
-        """
-        Train the model using Adam optimizer and CrossEntropyLoss.
-
-        Returns:
-        None
-        """
-
+    def train_model(self, train_loader):
+        """Train the model with progress tracking using tqdm."""
         print(f"""
               
                 ****************************************
-                ||=    Training PneumoniaClassifier
+                ||=    Training MelanomaClassifier
                 ||=    -------------------------------
                 ||=    Batch size: {self.batch_size}
                 ||=    Learning rate: {self.learning_rate}
-                ||=    Epochs: {self.num_epochs}
+                ||=    Epochs: {self.epochs}
+                ||=    Model path: {self.model_path}
                 ||=    Device: {self.device}
                 ||=    Model : {self.model_name}
                 ****************************************
         
-        """)
+            """)
+        self.net.to(self.device)
+        self.net.train()  # Set model to training mode
 
-
-        for epoch in range(self.num_epochs):
-            self.model.train()
-            progress_bar = tqdm.tqdm(self.train_loader, desc=f"Epoch {epoch + 1}/{self.num_epochs}", leave=True)
+        for epoch in range(self.epochs):
+            progress_bar = tqdm.tqdm(train_loader, desc=f"Epoch {epoch + 1}/{self.epochs}", leave=True)
             running_loss = 0.0
-            total_preds = 0
-            correct_preds = 0
 
             for i, (inputs, labels) in enumerate(progress_bar):
+                # Move data to the same device as the model
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
 
                 # Zero the parameter gradients
                 self.optimizer.zero_grad()
 
                 # Forward pass
-                outputs = self.model(inputs)
-                loss = self.criterion(outputs, labels)
+                outputs = self.net(inputs)
+                loss = self.loss_function(outputs, labels)
 
                 # Backward pass and optimization
                 loss.backward()
                 self.optimizer.step()
 
-                # Track loss and accuracy
-                _, predicted = torch.max(outputs, 1)
-                total_preds += labels.size(0)
-                correct_preds += (predicted == labels).sum().item()
+                # Update running loss
                 running_loss += loss.item()
 
-                progress_bar.set_description(f"Epoch {epoch + 1}/{self.num_epochs}, Loss: {loss.item():.4f}")
+                # Update progress bar description
+                progress_bar.set_description(f"Epoch {epoch + 1}/{self.epochs}, Loss: {loss.item():.4f}")
 
-            epoch_loss = running_loss / len(self.train_loader)
-            epoch_accuracy = 100 * correct_preds / total_preds
+            # Log epoch loss
+            epoch_loss = running_loss / len(train_loader)
+            logging.info(f"Epoch {epoch + 1}/{self.epochs} Loss: {epoch_loss:.4f}")
 
-            self.train_losses.append(epoch_loss)
-            self.train_accuracies.append(epoch_accuracy)
+    def evaluate_accuracy(self, data_loader, dataset_name="Validation"):
+        """Evaluates the accuracy on the given dataset."""
+        self.net.eval()  # Set model to evaluation mode
+        correct = 0
+        total = 0
 
-            print(f"Epoch {epoch + 1}/{self.num_epochs} - Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.2f}%")
-            self.logging.info(f"Epoch {epoch + 1}/{self.num_epochs} - Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.2f}%")
-
-    def evaluate(self, loader, type='Validation'):
-        """
-        Evaluate the model on a validation or test dataset.
-
-        Returns:
-        None
-        """
-        self.model.eval()
-        total_preds = 0
-        correct_preds = 0
-        running_loss = 0.0
         with torch.no_grad():
-            for inputs, labels in loader:
+            for inputs, labels in data_loader:
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.net(inputs)
+                predicted = torch.argmax(outputs, dim=1)
 
-                # Forward pass
-                outputs = self.model(inputs)
-                loss = self.criterion(outputs, labels)
-                running_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
-                total_preds += labels.size(0)
-                correct_preds += (predicted == labels).sum().item()
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0)
 
-        validation_accuracy = 100 * correct_preds / total_preds
-        self.validation_accuracies.append(validation_accuracy)
+        accuracy = (correct / total) * 100
+        logging.info(f"{dataset_name} Accuracy: {accuracy:.2f}%")
+        print(f"{dataset_name} Accuracy: {accuracy:.2f}%")
 
-        print(f"{type} Loss: {running_loss / len(loader):.4f}, {type} Accuracy: {validation_accuracy:.2f}%")
-        self.logging.info(f"{type} Loss: {running_loss / len(loader):.4f}, {type} Accuracy: {validation_accuracy:.2f}%")
-
-    def result(self):
-        """
-        Print final training loss and accuracy.
-        """
-        print("\n" + "=" * 50)
-        print(f"TRAINING RESULTS")
-        print("=" * 50)
-        print(f"Number of Epochs: {self.num_epochs}")
-        print("-" * 50)
-        print(f"Final Training Loss: {self.epoch_losses[-1]:.4f}")
-        print(f"Final Training Accuracy: {self.epoch_accuracies[-1]:.2f}%")
-        print("=" * 50)
-        self.logging.info("Final Training Accuracy: {self.epoch_accuracies[-1]:.2f}%")
-        self.logging.info("Final Training Loss: {self.epoch_losses[-1]:.4f}")
-
+    def summary_model(self):
+        """Displays a summary of the model architecture."""
+        return summary(self.net, input_size=(1, 224, 224), device=str(self.device))
 
 
     def plot_loss_accuracy(self):
@@ -154,7 +118,7 @@ class Train:
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
 
         # Plot training loss
-        ax1.plot(range(1, self.num_epochs + 1), self.epoch_losses, 'b-', label='Training Loss')
+        ax1.plot(range(1, self.num_epochs + 1), self.train_losses, 'b-', label='Training Loss')
         ax1.set_xlabel('Epochs')
         ax1.set_ylabel('Loss')
         ax1.set_title('Training Loss Over Time')
@@ -162,7 +126,7 @@ class Train:
         ax1.legend()
 
         # Plot training accuracy
-        ax2.plot(range(1, self.num_epochs + 1), self.epoch_accuracies, 'g-', label='Training Accuracy')
+        ax2.plot(range(1, self.num_epochs + 1), self.train_accuracies, 'g-', label='Training Accuracy')
         ax2.set_xlabel('Epochs')
         ax2.set_ylabel('Accuracy (%)')
         ax2.set_title('Training Accuracy Over Time')
@@ -170,7 +134,7 @@ class Train:
         ax2.legend()
 
         plt.tight_layout()
-        plt.savefig('plot_loss_accuracy.png')
+        plt.savefig('Images/plot_loss_accuracy.png')
         plt.show()
 
     def plot_train_test_accuracy(self):
@@ -194,14 +158,14 @@ class Train:
         plt.tight_layout()
 
         # Save the plot
-        plt.savefig('training_validation_accuracy.png')
+        plt.savefig('Images/training_validation_accuracy.png')
         plt.show()
 
     def save_model(self, path):
         """
         Save the trained model to a specified path.
         """
-        torch.save(self.model.state_dict(), path)
+        torch.save(self.model, path)
         print(f"Model saved successfully at {path}")
 
     def archit(self):
@@ -211,12 +175,12 @@ class Train:
         dummy_input = torch.randn(1, 3, 224, 224).to(self.device)
         summary(self.model, (3, 224, 224))
         print("Model summary saved.")
-        self.logging.info("Model summary saved.")
-        self.logging.info(f'su')
+        self.logger.info("Model summary saved.")
+        self.logger.info(f'su')
         dot = make_dot(self.model(dummy_input), params=dict(self.model.named_parameters()))
-        dot.render("model_architecture", format="png")
+        dot.render("Images/model_architecture", format="png")
         print("Model architecture saved as 'model_architecture.png'.")
-        self.logging.info("Model architecture saved as 'model_architecture.png'.")
+        self.logger.info("Model architecture saved as 'model_architecture.png'.")
 
     def plot_confusion_matrix(self, loader, classes, cmap=plt.cm.Blues):
         """
@@ -253,5 +217,5 @@ class Train:
         plt.xlabel('Predicted label')
         plt.tight_layout()
         plt.show()
-        plt.savefig('confusion_matrix.png')
-        self.logging.info("Confusion Matrix saved as 'confusion_matrix.png'.")
+        plt.savefig('Images/confusion_matrix.png')
+        self.logger.info("Confusion Matrix saved as 'confusion_matrix.png'.")
